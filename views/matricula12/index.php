b50994010f637df6eeaa148cff64fca1bc37c34a
Lázaro Javier <128192925+Cabero07@users.noreply.github.com>
2025-04-15 20:23:18 -0400

    fixing

--- views/matricula12/index.php
+++ views/matricula12/index.php
@@ -10,60 +10,88 @@ use yii\grid\GridView;
 /** @var app\models\Matricula12Search $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-
-$this->title = 'Matrícula Estudiantíl 12mo grado';
+$this->title = 'Matrícula Estudiantíl - 12mo Grado';
 ?>
-<div class="tbl-matricula-estudiantil12grado-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-    <?= Html::a('Añadir un nuevo estudiante', ['create'], ['class' => 'btn btn-warning']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            //'id',
-            'ci',
-            'apellido1ro',
-            'apellido2do',
-            'nombre1ro',
-            'nombre2do',
-            'telefono',
-            //'sexo',
-            //'color_piel',
-            //'proc_soc_padre',
-            //'proc_soc_madre',
-            //'calle',
-            //'numero',
-            //'apartamento',
-            //'piso',
-            //'entre_calle',
-            //'y_calle',
-            //'barrio',
-            //'municipio',
-            //'consejo_popular',
-            //'tomo',
-            //'folio',
-            //'numero_matricula',
-            //'grado',
-            'grupo',
-            //'deficiencias',
-            //'activo',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, TblMatriculaEstudiantil12grado $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                 }
-            ],
-        ],
-    ]); ?>
-
-
+<div class="container mt-4">
+    <div class="card shadow">
+        <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+            <h4 class="mb-0"><i class="fas fa-users"></i> <?= Html::encode($this->title) ?></h4>
+            <?= Html::a('<i class="fas fa-plus"></i> Añadir un nuevo estudiante', ['create'], ['class' => 'btn btn-warning']) ?>
+        </div>
+        <div class="card-body">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'options' => ['class' => 'table-responsive'],
+                'tableOptions' => ['class' => 'table table-bordered table-striped table-hover'],
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn', 'header' => '#'],
+                    [
+                        'attribute' => 'ci',
+                        'label' => 'CI',
+                        'headerOptions' => ['style' => 'width:120px; text-align:center;'],
+                        'contentOptions' => ['style' => 'text-align:center;'],
+                    ],
+                    [
+                        'attribute' => 'apellido1ro',
+                        'label' => 'Primer Apellido',
+                    ],
+                    [
+                        'attribute' => 'apellido2do',
+                        'label' => 'Segundo Apellido',
+                    ],
+                    [
+                        'attribute' => 'nombre1ro',
+                        'label' => 'Primer Nombre',
+                    ],
+                    [
+                        'attribute' => 'nombre2do',
+                        'label' => 'Segundo Nombre',
+                    ],
+                    [
+                        'attribute' => 'grupo',
+                        'headerOptions' => ['style' => 'width:80px; text-align:center;'],
+                        'contentOptions' => ['style' => 'text-align:center;'],
+                    ],
+                    [
+                        'attribute' => 'telefono',
+                        'headerOptions' => ['style' => 'width:150px; text-align:center;'],
+                        'contentOptions' => ['style' => 'text-align:center;'],
+                    ],
+                    [
+                        'class' => ActionColumn::className(),
+                        'header' => 'Acciones',
+                        'template' => '{view} {update} {delete}',
+                        'buttons' => [
+                            'view' => function ($url, $model, $key) {
+                                return Html::a('<i class="fas fa-eye"></i>', $url, [
+                                    'class' => 'btn btn-outline-primary btn-sm',
+                                    'title' => 'Ver detalles',
+                                ]);
+                            },
+                            'update' => function ($url, $model, $key) {
+                                return Html::a('<i class="fas fa-edit"></i>', $url, [
+                                    'class' => 'btn btn-outline-warning btn-sm',
+                                    'title' => 'Actualizar',
+                                ]);
+                            },
+                            'delete' => function ($url, $model, $key) {
+                                return Html::a('<i class="fas fa-trash"></i>', $url, [
+                                    'class' => 'btn btn-outline-danger btn-sm',
+                                    'title' => 'Eliminar',
+                                    'data' => [
+                                        'confirm' => '¿Estás seguro de que deseas eliminar este estudiante?',
+                                        'method' => 'post',
+                                    ],
+                                ]);
+                            },
+                        ],
+                        'urlCreator' => function ($action, TblMatriculaEstudiantil12grado $model, $key, $index, $column) {
+                            return Url::toRoute([$action, 'id' => $model->id]);
+                        },
+                    ],
+                ],
+            ]) ?>
+        </div>
+    </div>
 </div>
--- views/profesores/view.php
+++ views/profesores/view.php
@@ -6,46 +6,62 @@ use yii\widgets\DetailView;
 /** @var yii\web\View $this */
 /** @var app\models\TblProfesoresPreuniversitario $model */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Tbl Profesores Preuniversitarios', 'url' => ['index']];
+$this->title = $model->nombre1ro . ' ' . $model->apellido1ro;
+$this->params['breadcrumbs'][] = ['label' => 'Profesores Preuniversitarios', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="tbl-profesores-preuniversitario-view">
+<div class="container mt-4">
+    <div class="card shadow">
+        <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
+            <h4 class="mb-0"><i class="fas fa-user"></i> <?= Html::encode($this->title) ?></h4>
+            <div>
+                <?= Html::a('<i class="fas fa-edit"></i> Actualizar', ['update', 'id' => $model->id], ['class' => 'btn btn-warning']) ?>
+                <?= Html::a('<i class="fas fa-trash"></i> Eliminar', ['delete', 'id' => $model->id], [
+                    'class' => 'btn btn-danger',
+                    'data' => [
+                        'confirm' => '¿Estás seguro de que deseas eliminar este profesor?',
+                        'method' => 'post',
+                    ],
+                ]) ?>
+            </div>
+        </div>
+        <div class="card-body">
+            <?= DetailView::widget([
+                'model' => $model,
+                'options' => ['class' => 'table table-bordered table-hover'],
+                'attributes' => [
+                    [
+                        'label' => 'CI',
+                        'value' => $model->ci,
+                    ],
+                    [
+                        'label' => 'Nombre Completo',
+                        'value' => $model->nombre1ro . ' ' . $model->nombre2do . ' ' . $model->apellido1ro . ' ' . $model->apellido2do,
+                    ],
+                    [
+                        'label' => 'Teléfono',
+                        'value' => $model->telefono,
+                    ],
+                    [
+                        'label' => 'Género',
+                        'value' => $model->sexo,
+                    ],
+                    [
+                        'label' => 'Asignatura',
+                        'value' => $model->asignatura,
+                    ],
+                    [
+                        'label' => 'Dirección',
+                        'value' => $model->calle . ' #' . $model->numero . ', Apartamento: ' . $model->apartamento . ', Piso: ' . $model->piso . ', Barrio: ' . $model->barrio,
+                    ],
+                    [
+                        'label' => 'Entre Calles',
+                        'value' => $model->entre_calle . ' y ' . $model->y_calle,
+                    ],
+                ],
+            ]) ?>
+        </div>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'ci',
-            'apellido1ro',
-            'apellido2do',
-            'nombre1ro',
-            'nombre2do',
-            'telefono',
-            'sexo',
-            'asignatura',
-            'calle',
-            'numero',
-            'apartamento',
-            'piso',
-            'entre_calle',
-            'y_calle',
-            'barrio',
-        ],
-    ]) ?>
-
+    </div>
 </div>
