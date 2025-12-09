0f364a4a4cb187f5a5ed7d00cf4a475881482112
cabero <caberowow@gmail.com>
2025-12-09 01:17:30 -0500

    fixing

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -1,3 +1,8 @@
+<?php
+
+use yii\helpers\Url;
+
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="<?= Url::home() ?>" class="brand-link text-center bg-dark">
         <span class="brand-text font-weight-bold text-white" style="letter-spacing:1.5px;">
